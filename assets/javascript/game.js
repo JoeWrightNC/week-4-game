ded01a0d8967f8bc2c6c08f147be10820809d13d
Joe Wright <joewrightart@gmail.com>
2018-03-12 15:41:12 -0400

    fixed a few layout issues on selection screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,36 @@
 $(document).ready(function () {
 
-//setting variables and objects for game
-var
+//set variables for game
+var playerAP = 0;
+var playerHP = 0;
+var enemyAP = 0;
+var enemyHP = 0;
+var player ;
+var opponent ;
+var playerChoice = false ;
+var enemyChoice = false;
+var resetPlayerAP = 0;
+var resetPlayerHP = 0;
+var resetEnemyAP = 0;
+var resetEnemyHP = 0;
+var enemyLeft = 3;
+
+//selction js
+
+//click events, first select character, then opponent, then start game div goes live
+// (also add hover states to css)
+
+//battle js
+
+//put selected charater into player div and opponent into opponent div, other 2 oppo in top
+
+//battle attack and counter js
+
+//when one opponent is defeated select the next
+
+//you lose js
+
+//you win js
+
 
 })
