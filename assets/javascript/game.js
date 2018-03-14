842b0f39bc3eb83bfc33c815af86630568dbbfb2
Joe Wright <joewrightart@gmail.com>
2018-03-14 15:05:27 -0400

    finally got character selection in battle mode working, one bug squashed about 5 to go

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,37 @@ var resetEnemyAP = 0;
 var resetEnemyHP = 0;
 var enemyLeft = 3;
 
-//selction js
+//character objects
+var marioChar = {
+    aP: 15,
+    hP: 280,
+    source: "./assets/images/mario.png",
+}
+var linkChar = {
+    aP: 20,
+    hP: 250,
+    source: "./assets/images/link.png",
+}
+var foxChar = {
+    aP: 18,
+    hP: 230,
+    source: "./assets/images/fox.png",
+}
+var bowserChar = {
+  aP: 10,
+  hP: 280,
+  source: "./assets/images/Bowser.png",
+}
+var warioChar = {
+  aP: 18,
+  hP: 240,
+  source: "./assets/images/Wario.png",
+}
+var waluigiChar = {
+  aP: 15,
+  hP: 200,
+  source: "./assets/images/Waluigi.png",
+}
 
 //click events, first select character, then opponent 
 $(".selectionImg").on("click", function(playerSelection){
@@ -41,64 +71,127 @@ $(".selectionEnemyImg").on("click", function(enemySelection){
 });
 
 //start game button function
-
 $("#startBtn").on("click", function(startGame) {
   if (playerChoice === true && enemyChoice === true) {
     $("#selectionPage").hide();
     $("#battlePage").show();
     $("#losePage").hide();
     $("#winPage").hide();
-    $("#gameMusic").attr("src", "assets/audio/gameMusic.wav")
-    if ($(".selectionImg").attr("id") === "marioImg") {
-      playerAP = 15;
-      playerHP = 280;
-      console.log(playerAP);
-      $("#playerImg").attr("src", "./assets/images/Mario.png")
-    }
-    else if ($(".selectionImg").attr("id") === "linkImg") {
-      playerAP = 20;
-      playerHP = 250;
-      $("#playerImg").attr("src", "./assets/images/Link.png")
-      console.log(playerAP);
-    }
-    else if ($(".selectionImg").attr("id") === "foxImg") {
-      playerAP = 18;
-      playerHP = 230;
-      console.log(playerAP);
-      $("#playerImg").attr("src", "./assets/images/Fox.png")
-    }
-    if ($(".selectionEnemyImg").attr("id") === "bowserImg") {
-      enemyAP = 10;
-      enemyHP = 280;
-      console.log(enemyAP);
-      $("#opponentImg").attr("src", "./assets/images/Bowser.png")
-      $("#upNextImgOne").attr("src", "./assets/images/Wario.png")
-      $("#upNextImgTwo").attr("src", "./assets/images/Waluigi.png")
-    }
-    else if ($(".selectionEnemyImg").attr("id") === "warioImg") {
-      enemyAP = 18;
-      enemyHP = 240;
-      console.log(enemyAP);
-      $("#opponentImg").attr("src", "./assets/images/Wario.png")
-      $("#upNextImgOne").attr("src", "./assets/images/Bowser.png")
-      $("#upNextImgTwo").attr("src", "./assets/images/Waluigi.png")
-    }
-    else if ($(".selectionEnemyImg").attr("id") === "waluigiImg") {
-      enemyAP = 15;
-      enemyHP = 200;
-      console.log(enemyAP);
-      $("#opponentImg").attr("src", "./assets/images/Waluigi.png")
-      $("#upNextImgOne").attr("src", "./assets/images/Bowser.png")
-      $("#upNextImgTwo").attr("src", "./assets/images/Wario.png")
-    }
-    battleTime()
+    var source = $("#gameMusicSource");
+    source.attr("src", "assets/audio/gameSounds.wav");
+    charDefiner();
   }
   else {
     alert("Select your characters first!")
   }
 })
 
-//battle attack and counter mega function world sorry will
+function charDefiner() {
+  if ($(".selectionImg").attr("id") === "marioImg") {
+    playerAP = marioChar.aP;
+    playerHP = marioChar.hP;
+    $("#playerImg").attr("src", marioChar.source)
+    oppoDefiner();
+  }
+  else if ($(".selectionImg").attr("id") === "linkImg") {
+    playerAP = linkChar.aP;
+    playerHP = linkChar.hP;
+    $("#playerImg").attr("src", linkChar.source);
+    oppoDefiner();
+  }
+  else if ($(".selectionImg").attr("id") === "foxImg") {
+    playerAP = foxChar.aP;
+    playerHP = foxChar.hP;
+    $("#playerImg").attr("src", foxChar.source)
+    oppoDefiner();
+  }
+}
+
+function oppoDefiner() {
+  console.log(enemyLeft);
+  if (enemyLeft == 3) {
+    if ($(".selectionEnemyImg").attr("id") === "bowserImg") {
+      enemyAP = bowserChar.aP;
+      enemyHP = bowserChar.hP;
+      $("#opponentImg").attr("src", bowserChar.source)
+      $("#upNextImgOne").attr("src", warioChar.source)
+      $("#upNextImgTwo").attr("src", waluigiChar.source)
+    }
+    else if ($(".selectionEnemyImg").attr("id") === "warioImg") {
+      enemyAP = warioChar.aP;
+      enemyHP = warioChar.hP;
+      $("#opponentImg").attr("src", warioChar.source)
+      $("#upNextImgOne").attr("src", bowserChar.source)
+      $("#upNextImgTwo").attr("src", waluigiChar.source)
+    }
+    else if ($(".selectionEnemyImg").attr("id") === "waluigiImg") {
+      enemyAP = waluigiChar.aP;
+      enemyHP = waluigiChar.hP;
+      $("#opponentImg").attr("src", waluigiChar.source)
+      $("#upNextImgOne").attr("src", bowserChar.source)
+      $("#upNextImgTwo").attr("src", warioChar.source)
+    }
+  battleTime()
+  }
+  else if (enemyLeft == 2) {
+    $(".upNextImg").on("click", function (){
+    console.log(this)
+      if ($(this).attr("id") === "upNextImgOne") {
+        $("#opponentHP").text(enemyHP);
+        $("#opponentAP").text(enemyAP);
+        $("#upNextImgOne").appendTo("#opponent");
+        $("#opponentImg").hide();
+        $("#nextText").text("");
+        enemyAP = bowserChar.aP;
+        enemyHP = bowserChar.hP;
+        console.log("getting closer")
+        battleTime()
+      }
+      else if ($(this).attr("id") === "upNextImgTwo") {
+        $("#opponentHP").text(enemyHP);
+        $("#opponentAP").text(enemyAP);
+        $("#upNextImgTwo").appendTo("#opponent");
+        $("#opponentImg").hide();
+        $("#nextText").text("");
+        enemyAP = bowserChar.aP;
+        enemyHP = bowserChar.hP;
+        console.log("getting closer")
+        battleTime()
+      }
+    })
+  }
+  else if (enemyLeft == 1) {
+      $(".upNextImg").on("click", function (){
+      console.log("ya made it}")
+        if ($(this).attr("id") === "upNextImgOne") {
+          $("#opponentHP").text(enemyHP);
+          $("#opponentAP").text(enemyAP);
+          $("#upNextImgOne").appendTo("#opponent");
+          $("#opponentImg").hide();
+          $("#upNextImgTwo").hide();
+          $("#nextText").text("");
+          enemyAP = bowserChar.aP;
+          enemyHP = bowserChar.hP;
+          console.log("getting closer one")
+          battleTime()
+        }
+        else if ($(this).attr("id") === "upNextImgTwo") {
+          $("#opponentHP").text(enemyHP);
+          $("#opponentAP").text(enemyAP);
+          $("#upNextImgTwo").appendTo("#opponent");
+          $("#opponentImg").hide();
+          $("#upNextImgOne").hide();
+          $("#nextText").text("");
+          enemyAP = bowserChar.aP;
+          enemyHP = bowserChar.hP;
+          console.log("getting closer one")
+          battleTime()
+        }
+      })
+  }
+}
+
+//battle function
 
 function battleTime() {
   console.log("you made it to battle time")
@@ -111,18 +204,18 @@ function battleTime() {
       enemyHP = enemyHP - playerAP;
       $("#opponentHP").text(enemyHP);
       $("#opponentAP").text(enemyAP);
-      console.log(enemyHP);
       playerAP = playerAP + 3;
       $("#playerAP").text(playerAP);
-      if (enemyHP > 0 && playerHP > 0) {
+      var checker = playerHP - enemyAP ;
+      if (enemyHP > 0 && playerHP > checker) {
         playerHP = playerHP - enemyAP;
         $("#playerHP").text(playerHP);
-        if (playerHP <= 0) {
+      }
+      else if (playerHP <= 0) {
           $("#selectionPage").hide();
           $("#battlePage").hide();
           $("#losePage").show();
           $("#winPage").hide();
-        }
       }
       else if (enemyLeft > 1 && enemyHP <= 0) {
         enemyLeft--
@@ -139,81 +232,17 @@ function battleTime() {
   }
 }
 
+//function to choose next iterating in the battle function
+
 function chooseNext() {
   console.log("you made it to choose next");
+  console.log(enemyLeft)
   $("#attackBtn").unbind("click") 
   $("#nextText").text("Choose Next Opponent")
-  $("#upNextImgOne").on("click", function() {
-    if ($(".selectionEnemyImg").attr("id") === "bowserImg") {
-      enemyAP = 10;
-      enemyHP = 280;
-      $("#opponentHP").text(enemyHP);
-      $("#opponentAP").text(enemyAP);
-      console.log("heyyy");
-      console.log(enemyAP);
-      $("#upNextImgOne").appendTo("#opponent");
-      $("#opponentImg").hide();
-    }
-    else if ($(".selectionEnemyImg").attr("id") === "warioImg") {
-      enemyAP = 18;
-      enemyHP = 240;
-      console.log("heyyy");
-      console.log(enemyAP);
-      $("#opponentHP").text(enemyHP);
-      $("#opponentAP").text(enemyAP);
-      $("#upNextImgOne").appendTo("#opponent");
-      $("#opponentImg").hide();
-    }
-    else if ($(".selectionEnemyImg").attr("id") === "waluigiImg") {
-      enemyAP = 15;
-      enemyHP = 200;
-      $("#opponentHP").text(enemyHP);
-      $("#opponentAP").text(enemyAP);
-      console.log("heyyy");
-      console.log(enemyAP);
-
-      $("#upNextImgOne").appendTo("#opponent");
-      $("#opponentImg").hide();
-    }
-  $("#nextText").text("");
-  battleTime()
-  })
-  $("#upNextImgTwo").on("click", function() {
-    if ($(".selectionEnemyImg").attr("id") === "bowserImg") {
-      enemyAP = 10;
-      enemyHP = 280;
-      $("#opponentHP").text(enemyHP);
-      $("#opponentAP").text(enemyAP);
-      console.log("heyyy");
-      console.log(enemyAP);
-
-      $("#upNextImgTwo").appendTo("#opponent");
-      $("#opponentImg").hide();
-    }
-    else if ($(".selectionEnemyImg").attr("id") === "warioImg") {
-      enemyAP = 18;
-      enemyHP = 240;
-      $("#opponentHP").text(enemyHP);
-      $("#opponentAP").text(enemyAP);
-      console.log("heyyy");
-      console.log(enemyAP);
-
-      $("#upNextImgTwo").appendTo("#opponent");
-      $("#opponentImg").hide();
-    }
-    else if ($(".selectionEnemyImg").attr("id") === "waluigiImg") {
-      enemyAP = 15;
-      enemyHP = 200;
-      $("#opponentHP").text(enemyHP);
-      $("#opponentAP").text(enemyAP);
-      console.log("heyyy");
-      console.log(enemyAP);
-
-      $("#upNextImgTwo").appendTo("#opponent");
-      $("#opponentImg").hide();
-    }
-  $("#nextText").text("");
-  battleTime();
-  })
+  $("#opponentHP").empty();
+  $("#opponentAP").empty();
+  oppoDefiner()
 }
+
+
 })
